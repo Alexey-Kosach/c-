27d62f5930ece87b031f9271b805c8c3c3bfa049
Алексей Косач ТКИ-141 <alex_vk_007@mail.ru>
2026-03-14 11:06:54 +0300

    Update Cone.cpp

--- Cone.cpp
+++ Cone.cpp
@@ -1,11 +1,7 @@
-﻿#include <iostream>
+#define _USE_MATH_DEFINES // for C++
+#include <iostream>
 #include <cmath>
 #include "Cone.h"
-
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
 
 Cone::Cone(const double radius, const double height) {
     if (radius <= 0 || height <= 0) {
